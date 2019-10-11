35708b276037a50e2ca9066725fb5969cf5003b8
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-10-11 09:23:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,6 +6,18 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+@app.route("/about-us") #annotations tell which functions go with which request
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
+
 @app.route("/response")
 def render_response():
     favorite_color = request.args['color'] #get user's input for color input
@@ -15,6 +27,7 @@ def render_response():
         response = "Ok."
     return render_template('response.html', responseFromServer=response)
     
-    
 if __name__=="__main__":
     app.run(debug=False, port=54321)
+    
+    app = Flask(__name__)
