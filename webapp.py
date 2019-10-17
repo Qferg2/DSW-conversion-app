6984ea25fd51e87a7ce116ab286f21d419f7951e
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-10-17 09:27:17 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,8 +21,8 @@ def render_page3():
 @app.route("/response")
 def render_response():
     dollar_amount = request.args['Dollars'] #get user's input for color input
-    if dollar_amount == x:
-        response = x*19.16
+    response = dollar_amount*19.16
+    
     return render_template('response.html', responseFromServer=response)
     
 if __name__=="__main__":
