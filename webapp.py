5800c3201bfb7adfeda456d440167dc54915e13c
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-10-17 09:24:09 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,8 +21,8 @@ def render_page3():
 @app.route("/response")
 def render_response():
     dollar_amount = request.args['Dollars'] #get user's input for color input
-    if dollar_amount == 1:
-        response = "That's my favorite color!"
+    if dollar_amount == x:
+        response = x*19.16
     elif dollar_amount == 2:
         response = "BRUH"
     else:
